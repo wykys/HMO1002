ad39bb200733d30746a6fc28836c174969a7ccda
wykys <vvykys@gmail.com>
2023-10-01 01:24:11 +0200

    Fixed screenshot for ethernet

--- SCPI/screenshot.py
+++ SCPI/screenshot.py
@@ -15,33 +15,23 @@ def screenshot(name='img', color='color'):
 
     Device.send_cmd('HCOPy:DATA?')
 
-    if chr(Device.read_byte()) != '#':
+    if Device.read_response(1).decode('utf-8') != '#':
         log.err('# not found')
         exit(1)
 
-    size_len = Device.read_byte()
-    if size_len is None:
-        log.err('no data')
+    size_head = int(Device.read_response(1))
+    size_image = int(Device.read_response(size_head))
+
+    img = Device.read_response(size_image)
+    while len(img) < size_image:
+        tmp = Device.read_response(size_image - len(img))
+        if tmp is None:
+            break
+        img += tmp
+
+    if len(img != size_image):
+        log.err('bad image data')
         exit(1)
-    size_len = int(size_len - ord('0'))
-
-    bytes_len = []
-    for i in range(size_len):
-        tmp = Device.read_byte()
-        if tmp is None:
-            log.err('no data')
-            exit(1)
-        bytes_len.append(tmp)
-    bytes_len = int(bytes(bytes_len), 10)
-
-    img = []
-    for i in range(bytes_len):
-        tmp = Device.read_byte()
-        if tmp is None:
-            log.err('no data')
-            exit(1)
-        img.append(tmp)
-    img = bytes(img)
 
     fw = open(name + '.png', 'wb')
     fw.write(img)
