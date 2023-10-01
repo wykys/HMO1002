2dc0af1ecf40844fe6c82fe327a69ff4535718b0
wykys <vvykys@gmail.com>
2023-10-01 03:15:47 +0200

    Fixed export

--- SCPI/export.py
+++ SCPI/export.py
@@ -1,3 +1,4 @@
+import log
 from device import Device
 from matplotlib import pyplot as plt
 
@@ -6,12 +7,23 @@ def export():
     Device.send_cmd('FORMat ASCii, 32')
     Device.send_cmd('CHAN1:DATA?')
 
-    response = Device.read_response()
+    response = b''
+
+    while True:
+        tmp = Device.read_response(1024)
+        if tmp is None or tmp == b'':
+            break
+        response += tmp
+
+    if response == b'':
+        log.err('problem with data')
+        exit(1)
 
     t = []
     s = []
 
-    response = ''.join(map(lambda x: chr(x), response)).split('\n')[2:-2]
+    response = response.decode('utf-8').split('\n')[2:-2]
+
     for line in response:
         tmp = line.split(',')
         t.append(float(tmp[0]))
